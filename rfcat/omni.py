a1daa9eca911bf7b4eb16e4d460ae693fe492693
Dan Caron <dancaron@gmail.com>
2016-06-12 19:48:07 -0400

    clean up old variable names

--- rfcat/omni.py
+++ rfcat/omni.py
@@ -19,9 +19,9 @@ def flip_bytes(data):
 	bytes = map(lambda x: ord(x) ^ 0xff, data.decode("hex"))
 	return binascii.hexlify(bytearray(bytes))
 
-def quick_setup(device, baud, check=False):
+def quick_setup(device, bitrate, check=False):
 	"""quick_setup is used to setup rfcat to quickly decode omnipod signals"""
-	print "Usage: quick_setup(rfcat_device, baud, check_valid=False)"
+	print "Usage: quick_setup(rfcat_device, bitrate, check_valid=False)"
 	device.setFreq(433.91e6)
 	device.setMdmModulation(MOD_2FSK)
 	device.setPktPQT(1)
@@ -29,8 +29,8 @@ def quick_setup(device, baud, check=False):
 	device.setMdmSyncWord(0x54c3)
 	device.makePktFLEN(60)
 	device.setEnableMdmManchester(True)
-	device.setMdmDRate(baud)
-	print "Baud: %s" % baud
+	device.setMdmDRate(bitrate)
+	print "Bitrate: %s" % bitrate
 
 	while not keystop():
 		try:
