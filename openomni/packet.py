e429649110aaef95c565195efa4116f530a6de47
Pete Schwamb <pete@schwamb.net>
2016-11-24 11:40:42 -0600

    respect body len when decoding packets, add as_dict method, and add method to read in packet from formatted string

--- openomni/packet.py
+++ openomni/packet.py
@@ -1,5 +1,18 @@
 import json
 import crccheck
+import dateutil.parser
+from datetime import datetime
+
+
+# Serializer for datetime
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type not serializable")
+
 
 class Packet:
 
@@ -14,10 +27,14 @@ class Packet:
         PACKET_TYPE_ACK: "ACK",
         PACKET_TYPE_CON: "CON",
     }
+    PACKET_TYPES = dict(map(lambda x: [x[1],x[0]],PACKET_TYPE_STRINGS.items()))
 
     def __init__(self, data = ""):
         self.received_at = None
-        self.data = data
+        self.packet_type = None
+        self.body = None
+        self.body_len = None
+        self.message_type = None
         if len(data) < 10:
             return
         self.body = None
@@ -31,12 +48,12 @@ class Packet:
             self.packet_type_str = format(self.packet_type, '#05b')[2:]
 
         self.sequence = byte5 & 0b11111
-        self.pod_address_2 = data[5:9].encode("hex")
-        if len(self.data) > 13 and self.packet_type != Packet.PACKET_TYPE_ACK:
+        if len(data) > 13 and self.packet_type != Packet.PACKET_TYPE_ACK:
+            self.pod_address_2 = data[5:9].encode("hex")
             self.byte9 = ord(data[9])
             self.body_len = ord(data[10])
             self.message_type = data[11:13]
-            self.body = data[13:-1]
+            self.body = data[13:(13+min(23,self.body_len))]
         else:
             self.byte9 = None
             self.body_len = 0
@@ -57,14 +74,56 @@ class Packet:
     	bytes = map(lambda x: ord(x) ^ 0xff, data)
     	return bytearray(bytes).__str__()
 
+    def assign_from_string(self, line):
+        try:
+            elems = line.split(' ')
+            self.pod_address_2 = None
+            self.byte9 = None
+            self.received_at = dateutil.parser.parse(elems[0])
+            for elem in elems[1:]:
+                (key,v) = elem.split(':')
+                if key == "ID1":
+                    self.pod_address_1 = v
+                if key == "ID2":
+                    self.pod_address_2 = v
+                if key == "PTYPE":
+                    self.packet_type_str = v
+                    self.packet_type = self.PACKET_TYPES.get(v)
+                if key == "SEQ":
+                    self.sequence = int(v)
+                if key == "ID2":
+                    self.pod_address_2 = v
+                if key == "CRC":
+                    self.crc = int(v,16)
+                if key == "CON":
+                    self.body_continuation = v.decode('hex')
+                if key == "B9":
+                    self.byte9 = int(v,16)
+                if key == "BLEN":
+                    self.body_len = int(v,16)
+                if key == "MTYPE":
+                    self.message_type = v.decode('hex')
+                if key == "BODY":
+                    self.body = v.decode('hex')
+        except ValueError:
+            self.body = None
+        except OverflowError:
+            self.body = None
+
+        return self
+
+
     def tx_data(self):
         data = self.pod_address_1.decode('hex')
         data += chr((self.packet_type << 5) + self.sequence)
-        data += self.pod_address_2.decode('hex')
-        data += chr(self.byte9)
-        data += chr(self.body_len)
-        data += self.message_type
-        data += self.body
+        if self.packet_type == self.PACKET_TYPE_CON:
+            data += self.body_continuation
+        elif self.body is not None:
+            data += self.pod_address_2.decode('hex')
+            data += chr(self.byte9)
+            data += chr(self.body_len)
+            data += self.message_type
+            data += self.body
         data += chr(self.compute_crc_for(bytearray(data)))
         return data
 
@@ -77,7 +136,7 @@ class Packet:
         else:
             base_str = ""
 
-        base_str += "ID1:%s PTYPE:%s SEQ:%02s" % (
+        base_str += "ID1:%s PTYPE:%s SEQ:%02d" % (
                 self.pod_address_1,
                 self.packet_type_str,
                 self.sequence,
@@ -114,38 +173,53 @@ class Packet:
                 self.crc,
             )
 
+    def raw_hex(self):
+        return self.tx_data().encode('hex')
+
+    def as_dict(self):
+        if self.is_valid():
+            obj = {
+                "pod_address_1": self.pod_address_1,
+                "packet_type": self.packet_type,
+                "packet_type_str": self.packet_type_str,
+                "sequence": self.sequence,
+                "pod_address_2": self.pod_address_2,
+                "crc": self.crc,
+                "raw_packet": self.raw_hex(),
+            }
+            if self.byte9 is not None:
+                obj["byte9"] = self.byte9
+            if self.body is not None:
+                obj["body"] = self.body.encode('hex')
+                obj["body_len"] = self.body_len
+            if self.message_type is not None:
+                obj["message_type"] = self.message_type.encode('hex')
+            if self.received_at is not None:
+                obj["received_at"] = self.received_at
+        else:
+            obj = {}
+        return obj
+
     def as_json(self):
-        obj = {
-            "pod_address_1": self.pod_address_1,
-            "packet_type": self.packet_type,
-            "packet_type_str": self.packet_type_str,
-            "sequence": self.sequence,
-            "pod_address_2": self.pod_address_2,
-            "crc": self.crc,
-            "raw_packet": self.data.encode('hex'),
-        }
-        if self.byte9 is not None:
-            obj["byte9"] = self.byte9
-        if self.body is not None:
-            obj["body"] = self.body.encode('hex')
-            obj["body_len"] = self.body_len
-        if self.message_type is not None:
-            obj["message_type"] = self.message_type.encode('hex')
-        if self.received_at is not None:
-            obj["received_at"] = self.received_at.isoformat()
-        return json.dumps(obj, sort_keys=True,indent=4, separators=(',', ': '))
+        return json.dumps(self.as_dict(), default=json_serial, sort_keys=True, indent=4, separators=(',', ': '))
 
     def is_valid(self):
-        if self.data == None:
+        if self.packet_type is None:
             return False
-        if len(self.data) < 10:
-            return False
-        big_body_ok = self.body_len > 23 and len(self.body) == 23
-        small_body_ok = self.body_len == 0 or self.body_len == len(self.body)
-        return self.crc_ok() and (big_body_ok or small_body_ok)
+        if self.packet_type == self.PACKET_TYPE_CON:
+            body_ok = len(self.body_continuation) >= 1
+        elif self.packet_type == self.PACKET_TYPE_ACK:
+            body_ok = True
+        else:
+            if self.body is None:
+                return False
+            big_body_ok = self.body_len > 23 and len(self.body) == 23
+            small_body_ok = self.body_len == 0 or self.body_len == len(self.body)
+            body_ok = (big_body_ok or small_body_ok)
+        return self.crc_ok() and body_ok
 
     def compute_crc_for(self, data):
         return crccheck.crc.Crc8.calc(data)
 
     def crc_ok(self):
-        return self.crc == self.compute_crc_for(bytearray(self.data[:-1]))
+        return self.crc == self.compute_crc_for(bytearray(self.tx_data()[:-1]))
