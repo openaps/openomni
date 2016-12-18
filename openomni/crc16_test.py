12cd96c0df0e5d360d40ecf1b703adf532693c3d
Pete Schwamb <pete@schwamb.net>
2016-12-17 18:22:42 -0600

    Adding message class

---
+++ openomni/crc16_test.py
@@ -0,0 +1,9 @@
+import unittest
+import crc16
+
+class CrcTestCase(unittest.TestCase):
+
+    def test_crc_computation(self):
+        msg = "1f01482a10030e0100".decode('hex')
+        crc = crc16.calc(msg)
+        self.assertEqual(crc, 0x802c)
