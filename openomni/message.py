1c7d2381398c68f924b1ace826fb080b7d01dae7
Pete Schwamb <pete@schwamb.net>
2016-12-17 21:07:06 -0600

    Generate ack packet

--- openomni/message.py
+++ openomni/message.py
@@ -1,8 +1,10 @@
 import crc16
+from packet import Packet
 
 class Message(object):
-    def __init__(self, pod_id, byte9, body=""):
+    def __init__(self, pod_id, start_seq, byte9, body=""):
         self.pod_id = pod_id
+        self.start_seq = start_seq
         self.byte9 = byte9
         self.body = body
 
@@ -15,3 +17,22 @@ class Message(object):
 
     def computed_crc(self):
         return crc16.calc(self.data_for_crc())
+
+    def computed_crc_bytes(self):
+        crc = self.computed_crc()
+        return chr(crc >> 8) + chr(crc & 0xff)
+
+    def packets(self):
+        if len(self.body) > 23:
+            raise RuntimeError("No support for multi-packet messages yet.")
+
+        packet = Packet()
+        packet.pod_address_1 = self.pod_id
+        packet.packet_type = Packet.PACKET_TYPE_PDM
+        packet.sequence = self.start_seq
+        packet.pod_address_2 = self.pod_id
+        packet.byte9 = self.byte9
+        packet.body = self.body[2:] + self.computed_crc_bytes()
+        packet.body_len = len(self.body)
+        packet.message_type = self.body[0:2]
+        return [packet]
