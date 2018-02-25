7eb2a899e22ccaa8396f26da2a0c6237ca3305a9
Pete Schwamb <pete@schwamb.net>
2018-02-25 00:19:12 -0600

    Starting on insulin schedule commands

--- openomni/crc16_test.py
+++ openomni/crc16_test.py
@@ -7,3 +7,8 @@ class CrcTestCase(unittest.TestCase):
         msg = "1f01482a10030e0100".decode("hex")
         crc = crc16.calc(msg)
         self.assertEqual(crc, 0x802c)
+
+    def test_crc_computation(self):
+        msg = "1f00ee8628030e0100".decode("hex")
+        crc = crc16.calc(msg)
+        self.assertEqual(crc, 0x00d8)
